804e37e24577b50b79de74a5fe2cc290a9001533
Zetnavrek <vykovi@gmail.com>
2018-06-27 21:28:15 -0500

    Se agrega la correcta funcionalidad de los botones LogIn y LOogOut asi como su deshabilitacion en tiempo

--- js/signin.js
+++ js/signin.js
@@ -15,14 +15,7 @@ $('#login').click(function () {
     firebase.auth()
         .signInWithPopup(provider)
         .then(function (result) {
-            var email = result.user.email;
-            var name = result.user.displayName;
-            var img = result.user.photoURL;
-
-            $('#exampleModal').modal("hide");
-            $('#exampleModal').attr("style", "display: none");
-
-            console.log(email, name, img);
+            $('#login').attr('style', 'display: none');
             
 
         })
@@ -31,15 +24,27 @@ $('#login').click(function () {
 document.getElementById('logout').addEventListener('click', function(){
     console.log('click');
     firebase.auth().signOut();
+    
 });
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
+        document.getElementById('make-me').style.display='block';
         document.getElementById('user-menu').style.display='block';
+        var email = result.user.email;
+            var name = result.user.displayName;
+            var img = result.user.photoURL;
+            /*$('#exampleModal').modal("hide");
+            $('#exampleModal').attr("style", "display: none");
+            console.log(email, name, img);*/
+            
+        
     // User is signed in.
     } else{
         console.log('no existe usuario');
         document.getElementById('user-menu').style.display='none';
+        document.getElementById('login').style.display='block';
+        document.getElementById('make-me').style.display='none';   
     }
 });
 
